a80addeed2b10007ebeedd2f374b0b548c42d8ea
tomstejskal <tom@balon.cz>
2023-11-03 18:10:24 +0100

    feat: vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from "vite";
-import { qwikVite } from "@builder.io/qwik/optimizer";
-import { qwikCity } from "@builder.io/qwik-city/vite";
+import {defineConfig} from "vite";
+import {qwikVite} from "@builder.io/qwik/optimizer";
+import {qwikCity} from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
-  return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
-    preview: {
-      headers: {
-        "Cache-Control": "public, max-age=600",
-      },
-    },
-  };
+    return {
+        plugins: [
+            qwikCity({trailingSlash: false}),
+            qwikVite(),
+            tsconfigPaths()
+        ],
+        preview: {
+            headers: {
+                "Cache-Control": "public, max-age=600",
+            },
+        },
+    };
 });
