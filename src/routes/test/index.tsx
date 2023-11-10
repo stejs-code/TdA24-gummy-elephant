a6be8fddf6a34486d5f0dae3a5d7f3c471fd0ac4
Vitezslav Sima <kviteksima@seznam.cz>
2023-11-10 22:24:02 +0100

    feat: test.jsx styles

--- src/routes/test/index.tsx
+++ src/routes/test/index.tsx
@@ -11,41 +11,44 @@ export default component$(() => {
                     alt={"Mgr. Petra Swil Plachá MBA"}/>
 
             </div>
-            <div class="w-3/4 p-4">
-    <h1 class="text-3xl font-Lalezar text-black text-3xl font-medium">Mgr. Petra Swil Plachá MBA</h1>
-    <h4 class="text-sm text-gray-600 font-Open Sans">Aktivní studentka / Předsedkyně spolku</h4>
-    <div class="grid grid-cols-2 gap-4 mt-4">
+            <div class="flex flex-col items-start px-100">
+    <div class="p-4">
+    <h1 class="text-blackCustom text-4xl not-italic font-normal">Mgr. Petra Swil Plachá MBA</h1>
+    <h4 class="text-blackCustom mt-2 text-base not-italic font-normal">Aktivní studentka / Předsedkyně spolku</h4>
+    </div>
+    <div class="mt-12 flex justify-between max-w-sm">
+        <div>
         <div class="flex items-center">
-            <div class="w-6 h-6 bg-gray-400 mr-2"></div>
-            <p class="text-FEC93E font-Open Sans font-semibold">+420 722 482 974</p>
+            <div class="w-6 h-6 bg-gray-400 mr-2"></div> {/*ICONS-not added yet*/}
+            <p class="text-yellowCustom text-base not-italic font-semibold">+420 722 482 974</p>
         </div>
         <div class="flex items-center">
             <div class="w-6 h-6 bg-gray-400 mr-2"></div>
-            <p class="text-FEC93E font-Open Sans font-semibold">placha@scg.cz</p>
+            <p class="text-yellowCustom text-base not-italic font-semibold">placha@scg.cz</p>
+            <p class="text-yellowCustom text-base not-italic font-semibold">predseda@scg.cz</p>
+        </div>
+        </div>
+        <div>
+        <div class="flex items-center">
+            <div class="w-6 h-6 bg-gray-400 mr-2"></div>
+            <p class="text-yellowCustom text-base not-italic font-semibold">Brno</p>
         </div>
         <div class="flex items-center">
             <div class="w-6 h-6 bg-gray-400 mr-2"></div>
-            <p class="text-FEC93E font-Open Sans font-semibold">predseda@scg.cz</p>
+            <p class="text-yellowCustom text-base not-italic font-semibold">1200 Kč/h</p>
         </div>
-        <div class="flex items-center">
-            <div class="w-6 h-6 bg-gray-400 mr-2"></div>
-            <p class="text-black font-Open Sans font-semibold">Brno</p>
-        </div>
-        <div class="flex items-center">
-            <div class="w-6 h-6 bg-gray-400 mr-2"></div>
-            <p class="text-black font-Open Sans font-semibold">1200 Kč/h</p>
         </div>
     </div>
     <div class="p-4">
-        <p class="text-Open Sans text-base text-justify">Baví mě organizovat věci. Ať už to bylo vyvíjení mobilních aplikací ve Futured, pořádání konferencí, spolupráce na soutěžích Prezentiáda, pIšQworky, Tour de App a Středoškolák roku, nebo třeba dobrovolnictví, vždycky jsem skončila u projektového managementu, rozvíjení soft-skills a vzdělávání. U studentských projektů a akcí jsem si vyzkoušela snad všechno od marketingu po logistiku a moc ráda to předám dál. Momentálně studuji Pdf MUNI a FF MUNI v Brně.</p>
+        <p class="mt-12 text-blackCustom text-justify text-sm not-italic font-normal">Baví mě organizovat věci. Ať už to bylo vyvíjení mobilních aplikací ve Futured, pořádání konferencí, spolupráce na soutěžích Prezentiáda, pIšQworky, Tour de App a Středoškolák roku, nebo třeba dobrovolnictví, vždycky jsem skončila u projektového managementu, rozvíjení soft-skills a vzdělávání. U studentských projektů a akcí jsem si vyzkoušela snad všechno od marketingu po logistiku a moc ráda to předám dál. Momentálně studuji Pdf MUNI a FF MUNI v Brně.</p>
     </div>
     <div class="p-4">
-        <div class="space-x-2">
-            <span class="text-72C7D3 bg-white px-2 py-1 rounded-full text-Open Sans font-semibold text-xs">Fundraising pro neziskové studentské projekty</span>
-            <span class="text-72C7D3 bg-white px-2 py-1 rounded-full text-Open Sans font-semibold text-xs">Projektový management, event management</span>
-            <span class="text-72C7D3 bg-white px-2 py-1 rounded-full text-Open Sans font-semibold text-xs">Mimoškolní aktivity</span>
-            <span class="text-72C7D3 bg-white px-2 py-1 rounded-full text-Open Sans font-semibold text-xs">Fundraising pro neziskové studentské projekty</span>
-            <span class="text-72C7D3 bg-white px-2 py-1 rounded-full text-Open Sans font-semibold text-xs">Projektový management, event management</span>
+        <div class="mt-12 flex flex-wrap">
+            <span class="mr-2 mb-1 text-teal-300 text-center text-xs not-italic font-semibold rounded-md border border-solid border-blueCustom py-1.5 px-2.5">Fundraising pro neziskové studentské projekty</span>
+            <span class="mr-2 mb-1 text-teal-300 text-center text-xs not-italic font-semibold rounded-md border border-solid border-blueCustom py-1.5 px-2.5">Fundraising pro neziskové studentské projekty</span>
+            <span class="mr-2 mb-1 text-teal-300 text-center text-xs not-italic font-semibold rounded-md border border-solid border-blueCustom py-1.5 px-2.5">Fundraising pro neziskové studentské projekty</span>
+            <span class="mr-2 mb-1 text-teal-300 text-center text-xs not-italic font-semibold rounded-md border border-solid border-blueCustom py-1.5 px-2.5">Fundraising pro neziskové studentské projekty</span>
+
         </div>
     </div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'yellowCustom': '#FEC93E',
+      'blueCustom': '#72C7D3',
+      'blackCustom': '#181818'
+    }
   },
   plugins: [
     function ({ addVariant }) {
