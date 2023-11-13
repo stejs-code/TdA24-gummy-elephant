8ce9afe4c8c969c4e0e189a899784e32851aa269
e45g <e45g.dev@gmail.com>
2023-11-13 20:38:11 +0100

    fix: fixed backend

--- src/routes/api/lecturers/[id]/index.tsx
+++ src/routes/api/lecturers/[id]/index.tsx
@@ -8,7 +8,7 @@ export const onGet:RequestHandler = async ({env, json, params}) =>{
     if (response.success){
         json(200, response)
     }else {
-        json(404, {"code": 404, "message": "User not found"})
+        json(404, {"code": 404, message: "User not found"})
     }
 }
 
@@ -16,18 +16,22 @@ export const onDelete:RequestHandler = async ({env, json, params}) =>{
     const response = (await new Lecturer(getMeilisearch(env)).get(params.id))
     if (response.success){
         await new Lecturer(getMeilisearch(env)).delete(params.id)
-        json(204, {"code": 204, "message": "User deleted"});
+        json(204, {"code": 204, message: "User deleted"});
     } else {
-        json(404, {"code": 404, "message": "User not found"})
+        json(404, {"code": 404, message: "User not found"})
     }
 }
 
 export const onPut:RequestHandler = async ({env, json, params, request}) =>{
     const response = (await new Lecturer(getMeilisearch(env)).get(params.id))
     if (response.success) {
-        json(200, await new Lecturer(getMeilisearch(env)).update({...await request.json(), uuid: params.id}))
+        try{
+            json(200, await new Lecturer(getMeilisearch(env)).update({...await request.json(), uuid: params.id}))
+        }catch (e) {
+            json(400, {code: 400, message: JSON.stringify(e)})
+        }
     }
     else{
-        json(404, {"code": 404, "message": "User not found"})
+        json(404, {"code": 404, message: "User not found"})
     }
 }
--- src/routes/api/lecturers/index.tsx
+++ src/routes/api/lecturers/index.tsx
@@ -1,8 +1,26 @@
 import type {RequestHandler} from "@builder.io/qwik-city";
-import {Lecturer} from "~/app/lecturer";
+import {createParam, Lecturer} from "~/app/lecturer";
 import {getMeilisearch} from "~/app/meilisearch";
+import {z} from "zod";
 
 export const onGet:RequestHandler = async ({env, json}) =>{
-    const response = (await new Lecturer(getMeilisearch(env)).list())
-    json(200, response.data)
+    json(200, (await new Lecturer(getMeilisearch(env)).list()).data)
+}
+export const onPost:RequestHandler = async ({env, json, request}) =>{
+    try{
+
+        const response = (await request.json())
+        const lecturer = new Lecturer(getMeilisearch(env))
+        if('picture_url' in response) {
+            if (!(z.string().url(response.picture_url))) {
+                json(400, {code: 400, message: "picture_url is not a valid URL"})
+                return
+            }
+        }
+        json(200, (await lecturer.create(createParam.parse(response))).data)
+
+
+    }catch (e) {
+        json(400, {code: 400, message: JSON.stringify(e)})
+    }
 }
