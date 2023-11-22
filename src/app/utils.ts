a96ac6bd5d6cd07f0fc165dfadd84ace8c9c8400
tomstejskal <tom@balon.cz>
2023-11-22 20:50:48 +0100

    fix: defer + error handling

---
+++ src/app/utils.ts
@@ -0,0 +1,18 @@
+import type {AbortMessage} from "@builder.io/qwik-city/middleware/request-handler";
+import {ApiError} from "~/app/apiError";
+
+export function defer(func: () => any) {
+    setTimeout(func, 1)
+}
+
+export function handleRequestHandlingError(e:any, json: (statusCode: number, data: any) => AbortMessage) {
+    if (e instanceof SyntaxError && e.message === "Unexpected end of JSON input") {
+        return json(400, {
+            code: 400,
+            message: "invalid json"
+        })
+    }
+
+    console.error(e)
+    return ApiError.internal().sendResponse(json)
+}
