27bcdf337d4ee550cdd6f54177d7293a2a7eab22
tomstejskal <tom@balon.cz>
2023-11-22 20:51:03 +0100

    feat: typing

--- src/app/zod.ts
+++ src/app/zod.ts
@@ -1,17 +1,22 @@
 import {z} from "zod";
 
-export const errorZod= z.object({
+export const errorZod = z.object({
     code: z.number(),
     message: z.string()
 })
+
 export const tagZod = z.object({
     uuid: z.string(),
     name: z.string()
 })
+
+export type TagType = z.infer<typeof tagZod>
+
 export const contactZod = z.object({
     telephone_numbers: z.array(z.string()),
     emails: z.array(z.string()),
 })
+
 export const lecturerZod = z.object({
     uuid: z.string().uuid(),
     title_before: z.string().optional(),
@@ -28,11 +33,18 @@ export const lecturerZod = z.object({
     contact: contactZod.optional()
 })
 
-export const updatedLectureZod = lecturerZod.partial()
-
 export type LecturerType = z.infer<typeof lecturerZod>
 
 export function zodErrorToString(zodError: z.ZodError) {
-    zodError.toString()
-
+    return zodError.issues.map(i => "\"" + i.path.join(".") + "\": " + i.message).join("; ")
 }
+
+export const createBody = lecturerZod.merge(z.object({
+    tags: z.array(tagZod.omit({
+        uuid: true
+    })).optional()
+})).omit({
+    uuid: true
+})
+
+export const updateLectureBodyZod = createBody.partial()
