256c3c7bd68ff63f832a7ccc475e88242f5e6304
e45g <e45g.dev@gmail.com>
2024-02-20 17:04:17 +0100

    flag: almost there~

--- src/routes/api/lecturers/index.tsx
+++ src/routes/api/lecturers/index.tsx
@@ -2,8 +2,9 @@ import type {RequestHandler} from "@builder.io/qwik-city";
 import {ApiError} from "~/app/apiError";
 import {handleRequestHandlingError} from "~/app/utils";
 import {Context} from "~/app/context";
-import {listLecturers} from "~/app/lecturer";
+import {createLecturer, listLecturers} from "~/app/lecturer";
 
+var i = 0;
 export const onGet: RequestHandler = async ({env, json}) => {
     try {
         const ctx = new Context({env})
@@ -18,17 +19,18 @@ export const onGet: RequestHandler = async ({env, json}) => {
 }
 
 
-export const onPost: RequestHandler = async ({json}) => {
+export const onPost: RequestHandler = async ({json, env, request}) => {
     try {
-        json(401, {error: 401, message: "Auth required"})
-        // const ctx = new Context({env})
-        //
-        // const response = await createLecturer(ctx, await request.json())
-        //
-        // if (response instanceof ApiError) return response.sendResponse(json)
-        //
-        // json(200, response)
-        //
+        if(i!=3){
+            json(401, {error: 401, message: "Auth required"})
+        }else{
+            const ctx = new Context({env})
+            const response = await createLecturer(ctx, await request.json())
+            if (response instanceof ApiError) return response.sendResponse(json)
+            json(200, response)
+
+        }
+        
     } catch (e) {
         handleRequestHandlingError(e, json)
     }
