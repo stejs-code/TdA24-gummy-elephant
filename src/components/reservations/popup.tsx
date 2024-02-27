393014f0226336a996627bdce7e28abca238783e
WintiS <kviteksima@seznam.cz>
2024-02-27 14:31:21 +0100

    feat: responsive popup{modal]

--- src/components/reservations/popup.tsx
+++ src/components/reservations/popup.tsx
@@ -20,9 +20,9 @@ export const Popup =  component$((props:Props) => {
 
     return(
         <Modal bind:show={modalVisible}
-               class={"sheet shadow-dark-medium overflow-y-hidden max-h-[100vh] fixed right-0 inset-y-0 my-0 mr-0 h-[100vh] max-w-[60rem] rounded-l-md border-0 bg-white p-6 text-slate-950 backdrop:backdrop-blur backdrop:backdrop-brightness-100"}>
+               class={"overflow-y-scroll sheet shadow-dark-medium max-h-[100vh] fixed right-0 inset-y-0 my-0 mr-0 h-[100vh] max-w-full md:max-w-[60rem] rounded-l-md border-0 bg-white p-0 sm:p-6 text-slate-950 backdrop:backdrop-blur backdrop:backdrop-brightness-100"}>
 
-            <div class={"px-6 py-2.5"}>
+            <div class={"px-3 pt-3 sm:px-6 sm:py-2.5"}>
                 <div class={"flex items-center justify-between mb-6"}>
                     <h2 class={"text-2xl font-bold"}>Rezervace 16 - 19</h2>
                     <div class="p-1 cursor-pointer"
@@ -31,13 +31,13 @@ export const Popup =  component$((props:Props) => {
                          }}><LuX/></div>
                 </div>
                 <div class={"flex items-center gap-5 mb-5"}>
-                    <div>
+                    <div class={"w-1/2"}>
                         <p class={"mb-1"}>Jméno:</p>
-                        <input type="text" value={"Marie"} disabled={true} class={"px-3 py-1.5 border rounded-lg border-gray-300 bg-white"}/>
+                        <input type="text" value={"Marie"} disabled={true} class={"px-3 py-1.5 border rounded-lg border-gray-300 bg-white w-full"}/>
                     </div>
-                    <div>
+                    <div class={"w-1/2"}>
                         <p class={"mb-1"}>Příjmení:</p>
-                        <input type="text" value={"Juchalkova"} disabled={true}  class={"px-3 py-1.5 border rounded-lg border-gray-300 bg-white"}/>
+                        <input type="text" value={"Juchalkova"} disabled={true}  class={"px-3 py-1.5 border rounded-lg border-gray-300 bg-white w-full"}/>
                     </div>
                 </div>
                 <div class={"mb-5"}>
@@ -50,9 +50,9 @@ export const Popup =  component$((props:Props) => {
                         <input type="tel" value={"+420 733 333 233"} disabled={true} class={"px-3 py-1.5 border rounded-lg border-gray-300 bg-white w-full "}/>
                     </div>
                     <div class={"w-1/2"}>
-                        <p class={"mb-1"}>Tag:</p>
-                        <select id={"time"} class={"px-3 py-1.5 border rounded-lg border-gray-300 bg-white w-full cursor-pointer"} disabled={true}>
-                            <option value="BDSM">Javascript</option>
+                        <p class={"mb-1"}>Vybraný Tag:</p>
+                        <select id={"time"} class={"px-3 py-1.5 border rounded-lg border-gray-300 bg-white w-full "} disabled={true}>
+                            <option value="BDSM">Javascript - mentoring</option>
                             <option value="BDSM">BDSM</option>
                         </select>
                     </div>
@@ -60,7 +60,7 @@ export const Popup =  component$((props:Props) => {
                 <div class={"flex items-center gap-5 mb-5"}>
                     <div class={"w-1/2"}>
                         <p class={"mb-1"}>Datum:</p>
-                        <input type="date" value={"Jana"} disabled={false} class={"px-3 py-1.5 border rounded-lg border-gray-300 bg-white w-full "}/>
+                        <input type="date" value={"Jana"} disabled={false} class={"px-3 py-1.5 border rounded-lg border-gray-300 bg-white w-full cursor-pointer"}/>
                     </div>
                     <div class={"w-1/2"}>
                         <p class={"mb-1"}>Čas:</p>
@@ -75,8 +75,8 @@ export const Popup =  component$((props:Props) => {
                     <textarea name="note" id="note" cols={50} rows={6} class={"px-3 py-1.5 border rounded-lg border-gray-300 bg-white w-full resize-none text-sm"} value={"Prosím tě, dopřej mi radost z chutného pokrmu, který mi zahřeje srdce a okoření můj den. Tvá kulinářská kouzla mi přinesou nejen fyzickou sytost, ale i kulturní dobrodružství prostřednictvím vůně a chuťových zážitků. Děkuji ti za každý laskavý okamžik sdílený skrze výtečné jídlo."}></textarea>
                 </div>
                 <div>
-                    <PrimaryButton type="submit" class={"flex-shrink-0 w-sm cursor-pointer"}>
-                            <span class={"hidden sm:inline"}
+                    <PrimaryButton type="submit" class={"flex-shrink-0 w-sm"}>
+                            <span class={"inline"}
                                   onClick$={() => {
                                       modalVisible.value = false
                                   }}
