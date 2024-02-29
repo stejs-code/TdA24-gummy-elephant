a79e1056e088c76c7f348bf5ea91902719db5c03
WintiS <kviteksima@seznam.cz>
2024-02-29 16:03:30 +0100

    fix: nav z index

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -15,7 +15,7 @@ export const Navigation = component$<NavigationProps>(({user, notification}) => 
     const location = useLocation()
 
     return (
-        <div class={"sticky top-0"}>
+        <div class={"sticky top-0 z-30"}>
             <nav class={"w-full bg-white border-b border-slate-200 mb-2"}>
                 <div class={"flex mx-auto max-w-6xl p-4 pb-3 items-center"}>
                     <Link href={"/"} prefetch={true}>
